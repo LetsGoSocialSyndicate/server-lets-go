2a6a9cd7aeddcb9b0bc462939ff6eefc760e8cea
slledru <slledru@msn.com>
2018-06-05 14:11:26 -0600

    starting to implement users route

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,21 @@
  */
 const express = require('express')
 const router = express.Router()
+const UserService = require('../database/services/userService')
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource')
+  const userService = new UserService()
+  userService.getList()
+    .then((rows) => res.json(rows))
+    .catch((err) => next(err))
 })
 
 router.get('/:id', (req, res, next) => {
-  res.send('respond with a resource')
-})
-
-router.post('/', (req, res, next) => {
-  res.send('respond with a resource')
+  const userService = new UserService()
+  userService.get(req.params.id)
+    .then((row) => res.json(row))
+    .catch((err) => next(err))
 })
 
 router.patch('/:id', (req, res, next) => {
