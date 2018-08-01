56f396f397596e17022b3ee2336de652959e8e29
Tanya <mellene@gmail.com>
2018-08-01 13:26:07 -0600

    autologin

--- routes/login.js
+++ routes/login.js
@@ -1,35 +1,48 @@
 /* Copyright 2018, Socializing Syndicate Corp. */
 const express = require('express')
-const router = express.Router()
-const knex = require('../knex')
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
+const boom = require('boom')
 const UserService = require('../database/services/userService')
 const TokenService = require('../database/services/tokenService')
 const { sendEmail } = require('../utilities/mailUtils')
-const {invalidInput, constructFailure} = require('../utilities/routeUtil')
-const {NOT_VERIFIED, BAD_EMAIL_OR_PASSWORD, DATABASE_ERROR} = require('../utilities/constants')
+const { getValidToken } = require('../utilities/jwtUtil')
+const { invalidInput, constructFailure } = require('../utilities/routeUtil')
+const {
+  NOT_VERIFIED,
+  BAD_EMAIL_OR_PASSWORD,
+  BAD_TOKEN,
+  DATABASE_ERROR,
+  TOKEN_EXPIRED
+} = require('../utilities/constants')
 
 require('dotenv').config()
 
+const router = express.Router()
+
 const ONE_DAY = 1000 * 3600 * 24
 
+const generateToken = payload => {
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '30d' })
+}
+
 const verifyUserInDatabase = (userService, email) => {
-  return userService.getByEmail(email).then(result => {
-    return "ok"
+  return userService.getByEmail(email).then(() => {
+    return 'ok'
   }).catch(err => {
     if (err.typeof === boom.notFound) {
       throw boom.badRequest(
         'The email you have entered is not found.',
-        {errorType: BAD_EMAIL_OR_PASSWORD})
+        { errorType: BAD_EMAIL_OR_PASSWORD }
+      )
     }
     throw err
   })
 }
 
 router.post('/', (req, res, next) => {
-  const {email, password} = req.body
+  const { email, password } = req.body
   // console.log("login:", email, password)
   if (email && password) {
     const userService = new UserService()
@@ -37,12 +50,8 @@ router.post('/', (req, res, next) => {
       // console.log(result)
       if (bcrypt.compareSync(password, result.hashed_password)) {
         if (result.verified_at) {
-          const payload = {
-            email,
-            userId: result.id
-          }
-          const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1d'})
-          res.status(200).json({user: result, token})
+          const token = generateToken({ email, userId: result.id })
+          res.status(200).json({ user: result, token })
         } else {
           next(constructFailure(NOT_VERIFIED, 'Your account has not been verified.', 401))
         }
@@ -50,21 +59,22 @@ router.post('/', (req, res, next) => {
         next(constructFailure(BAD_EMAIL_OR_PASSWORD, 'Bad email or password.', 401))
       }
     }).catch((err) => {
-      console.log("login error:", err)
+      console.log('login error:', err)
       let status = 500
-      let payload = constructFailure(DATABASE_ERROR, 'Error while looking up user in database', status)
+      let payload = constructFailure(
+        DATABASE_ERROR,
+        'Error while looking up user in database',
+        status
+      )
       if (err.isBoom) {
-        const data = err.data
-          ? err.data
-          : {}
+        const data = err.data ? err.data : {}
         status = err.output.statusCode
         if (status === 404) {
-          payload = constructFailure(BAD_EMAIL_OR_PASSWORD, 'Bad email or password.', 401)
+          payload = constructFailure(
+            BAD_EMAIL_OR_PASSWORD, 'Bad email or password.', 401
+          )
         } else {
-          payload = {
-            ...data,
-            ...err.output.payload
-          }
+          payload = { ...data, ...err.output.payload }
         }
       }
       next(payload)
@@ -72,14 +82,25 @@ router.post('/', (req, res, next) => {
   } else {
     next(invalidInput('Username and/or password was not set'))
   }
-  console.log("End of POST /login")
+})
+
+router.get('/refresh', (req, res, next) => {
+  try {
+    const token = getValidToken(req)
+    // console.log('refresh OLD:', token)
+    const newToken = generateToken({ email: token.email, userId: token.userId })
+    // console.log('refresh NEW:', newToken)
+    res.status(200).json({ token: newToken })
+  } catch (error) {
+    next(constructFailure(BAD_TOKEN, error, 403))
+  }
 })
 
 router.post('/code_for_pswd', (req, res, next) => {
   // console.log("HEADERS:", req.headers)
-  const {email} = req.body
+  const { email } = req.body
   if (!email) {
-    next(invalidInput("Email cannot be blank"))
+    next(invalidInput('Email cannot be blank'))
     return
   }
   const userService = new UserService()
@@ -87,29 +108,36 @@ router.post('/code_for_pswd', (req, res, next) => {
 
   verifyUserInDatabase(userService, email).then(() => {
     const tokenEntry = {
-      email: email,
-      token: crypto.randomBytes(3).toString('hex') //we will need at least 6 in migrations
+      email,
+      token: crypto.randomBytes(3).toString('hex') // we will need at least 6 in migrations
     }
     // console.log(tokenEntry);
     // TODO: check if token exists and return error.
     return tokenService.insert(tokenEntry).catch(err => {
       throw err
-    }).then(result => {
-      return sendEmail(email, 'Let\'s Go: Create New Password', `<html>
-              <head>
-                <title>Let's Go: Create New Password</title>
-              </head>
-              <body>
-                <p>
-                  Hello,<br><br>
-                  Please enter the code below on the screen and press OK to continue.<br><br>
-                  The confirmation code is <b>${tokenEntry.token}</b>.<br>
-                </p>
-              </body>
-            </html>`, 'A verification code has been sent to ' + email + '.', 'Error while sending verification email')
-    }).then((result) => res.json(result)).catch((err) => next(err))
+    }).then(() => {
+      return sendEmail(
+        email,
+        'Let\'s Go: Create New Password',
+        `<html>
+           <head>
+             <title>Let's Go: Create New Password</title>
+           </head>
+           <body>
+             <p>
+               Hello,<br><br>
+               Please enter the code below on the screen and press OK to continue.<br><br>
+               The confirmation code is <b>${tokenEntry.token}</b>.<br>
+             </p>
+           </body>
+         </html>`,
+        `A verification code has been sent to ${email}.`,
+        'Error while sending verification email'
+      )
+    }).then((result) => res.json(result))
+      .catch((err) => next(err))
   }).catch((err) => {
-    console.log("code_for_pswd error:", err)
+    console.log('code_for_pswd error:', err)
     let status = 500
     let payload = constructFailure(DATABASE_ERROR, 'Error while accessing user database', status)
     if (err.isBoom) {
@@ -128,10 +156,10 @@ router.post('/code_for_pswd', (req, res, next) => {
 
 router.get('/:token', (req, res, next) => {
   // console.log('Params', req.params.token)
-  const {token} = req.params
+  const { token } = req.params
 
   if (!token) {
-    next(invalidInput(res, "Invalid link with missing token"))
+    next(invalidInput(res, 'Invalid link with missing token'))
     return
   }
 
@@ -141,9 +169,9 @@ router.get('/:token', (req, res, next) => {
   tokenService.get(token).then(resultTokenRecord => {
     const created_at = new Date(resultTokenRecord.created_at)
     const tokenAge = Date.now() - created_at
-    //change later to 10 minutes
+    // change later to 10 minutes
     if (tokenAge > ONE_DAY) {
-      tokenService.delete(token).then(resultTokenDelete => {
+      tokenService.delete(token).then(() => {
         next(constructFailure(TOKEN_EXPIRED, 'The token has expired.', 401))
         return
       })
@@ -153,17 +181,18 @@ router.get('/:token', (req, res, next) => {
         next(constructFailure(NOT_VERIFIED, 'Your account has not been verified.', 401))
         return
       }
-      tokenService.delete(token).then(resultTokenDelete => {
+      tokenService.delete(token).then(() => {
         const payload = {
           email: resultUserRecord.email,
           userId: resultUserRecord.id
         }
-        const sessionToken = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1d'})
-        return res.status(200).send({user: resultUserRecord, token: sessionToken})
+        const sessionToken =
+          jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1d' })
+        return res.status(200).send({ user: resultUserRecord, token: sessionToken })
       })
     })
   }).catch((err) => {
-    console.log("confirmation request error:", err)
+    console.log('confirmation request error:', err)
     let status = 500
     let payload = constructFailure(DATABASE_ERROR, 'Error while confirming request.', status)
     if (err.isBoom) {
--- utilities/jwtUtil.js
+++ utilities/jwtUtil.js
@@ -2,53 +2,40 @@
  * Copyright 2018, Socializing Syndicate Corp.
  */
 const jwt = require('jsonwebtoken')
-const boom = require('boom')
-
-function handleResponse(op, email, httpRes, user) {
-  // console.log('email: ', email)
-  const token = jwt.sign({
-    'email': email
-  }, process.env.SECRET_KEY)
-  // console.log('token: ', token)
-  httpRes.setHeader('Set-Cookie', `token=${token};`)
-}
-
-function noCaching(res) {
-  res.setHeader('Cache-Control', `no-cache, no-store, must-revalidate`)
-}
 
 // for Logged in request:
-function verifyToken(req, res, next) {
+function getValidToken(req) {
   const { authorization } = req.headers
-  // console.log('verifyToken - authorization: ', authorization)
-
-  try {
-    if (!authorization) {
-      throw new Error('Authorization header not found')
-    }
-    const authParsed = authorization.split(' ')
-    if (authParsed.length !== 2) {
-      throw new Error('Authorization header is corrupt')
-    }
-    jwt.verify(authParsed[1], process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        throw new Error('JWT token is invalid')
-      }
-      else {
-        // console.log('decoded', decoded)
-        req.email = decoded.email
-        req.userId = decoded.userId
-        next()
-      }
-    })
+  // console.log('getValidToken - headers:', req.headers)
+  // console.log('getValidToken - authorization:', authorization)
+  if (!authorization) {
+    throw new Error('Authorization header not found')
   }
-  catch (error) {
+  const authParsed = authorization.split(' ')
+  if (authParsed.length !== 2) {
+    throw new Error('Authorization header is corrupt')
+  }
+  try {
+    return jwt.verify(authParsed[1], process.env.JWT_SECRET)
+  } catch (error) {
+    throw new Error('JWT token is invalid')
+  }
+}
+
+function verifyToken(req, res, next) {
+  try {
+    const token = getValidToken(req)
+    // console.log('verifyToken:', token)
+    req.email = token.email // eslint-disable-line no-param-reassign
+    req.userId = token.userId // eslint-disable-line no-param-reassign
+    next()
+  } catch (error) {
+    // console.log('verifyToken error:', error)
     res.status(403).json({ error })
   }
 }
 
 module.exports = {
-  handleResponse,
-  noCaching,
+  getValidToken,
   verifyToken
 }
