36e92af129d881cde8713627fbba85f51253f280
Tanya <mellene@gmail.com>
2018-08-03 16:03:18 -0600

    fixing visual bugs

--- routes/signup.js
+++ routes/signup.js
@@ -1,22 +1,37 @@
 /* Copyright 2018, Socializing Syndicate Corp. */
 const express = require('express')
+
 const router = express.Router()
-const nodemailer = require('nodemailer')
-const bodyParser = require('body-parser')
-const exphbs = require('express-handlebars')
+
+// const nodemailer = require('nodemailer')
+// const bodyParser = require('body-parser')
+// const exphbs = require('express-handlebars')
 const crypto = require('crypto')
 const boom = require('boom')
-const knex = require('../knex')
+// const knex = require('../knex')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 const moment = require('moment')
 const UserService = require('../database/services/userService')
 const ProfileImageService = require('../database/services/profileImageService')
 const TokenService = require('../database/services/tokenService')
-const { ALREADY_EXISTS, ALREADY_EXISTS_UNVERIFIED, PASSWORD_REGULAR_EXP,
-  DATABASE_ERROR, SENDING_MAIL_ERROR, TOKEN_EXPIRED } = require('../utilities/constants')
-const { constructSuccess, constructFailure, invalidInput } = require('../utilities/routeUtil')
-const { USER_ROLE_REGULAR, DEFAULT_USER_PROFILE_IMAGE } = require('../database/services/constants')
+const {
+  ALREADY_EXISTS,
+  ALREADY_EXISTS_UNVERIFIED,
+  PASSWORD_REGULAR_EXP,
+  DATABASE_ERROR,
+  // SENDING_MAIL_ERROR,
+  TOKEN_EXPIRED
+} = require('../utilities/constants')
+const {
+  // constructSuccess,
+  constructFailure,
+  invalidInput
+} = require('../utilities/routeUtil')
+const {
+  USER_ROLE_REGULAR,
+  DEFAULT_USER_PROFILE_IMAGE
+} = require('../database/services/constants')
 const { sendEmail } = require('../utilities/mailUtils')
 const { checkToken } = require('../utilities/dbUtils')
 
@@ -25,37 +40,40 @@ const verifyUserNotInDatabase = (userService, email) => {
     throw boom.badRequest(
       'The email you have entered is already ' +
       'associated with another account.', {
-      errorType: result.verified_at
-        ? ALREADY_EXISTS
-        : ALREADY_EXISTS_UNVERIFIED
-    })
+        errorType: result.verified_at
+          ? ALREADY_EXISTS
+          : ALREADY_EXISTS_UNVERIFIED
+      })
   }).catch(err => {
     if (err.typeof !== boom.notFound) {
       throw err
     }
   })
 }
-
-const isOlderThan18 = (birthday) => {
-  const today = new Date()
-  // console.log(birthday, today)
-  if (today.getFullYear() - birthday.getFullYear() > 18) {
-    return true
-  }
-  if (today.getFullYear() - birthday.getFullYear() < 18) {
-    return false
-  }
-  if (today.getMonth() > birthday.getMonth()) {
-    return true
-  }
-  if (today.getMonth() < birthday.getMonth()) {
-    return false
-  }
-  if (today.getDate() >= birthday.getDate()) {
-    return true
-  }
-  return false
+const verifyAge = (birthday) => {
+  const minAge = moment().utc().subtract(18, 'years').format('YYYY-MM-DD')
+  return moment(birthday).isBefore(minAge)
 }
+// const isOlderThan18 = (birthday) => {
+//   const today = new Date()
+//   // console.log(birthday, today)
+//   if (today.getFullYear() - birthday.getFullYear() > 18) {
+//     return true
+//   }
+//   if (today.getFullYear() - birthday.getFullYear() < 18) {
+//     return false
+//   }
+//   if (today.getMonth() > birthday.getMonth()) {
+//     return true
+//   }
+//   if (today.getMonth() < birthday.getMonth()) {
+//     return false
+//   }
+//   if (today.getDate() >= birthday.getDate()) {
+//     return true
+//   }
+//   return false
+// }
 
 router.post('/', (req, res, next) => {
   // console.log("HEADERS:", req.headers)
@@ -69,25 +87,29 @@ router.post('/', (req, res, next) => {
     birthday
   } = req.body
   if (!birthday) {
-    next(invalidInput("Birthday cannot be blank"))
+    next(invalidInput('Birthday cannot be blank'))
     return
   }
   if (!email) {
-    next(invalidInput("Email cannot be blank"))
+    next(invalidInput('Email cannot be blank'))
     return
   }
   if (!password) {
-    next(invalidInput("Password cannot be blank"))
+    next(invalidInput('Password cannot be blank'))
     return
   }
-  if (process.env.PASSWORD_RULE !== 'DEVELOP') {
-    if (!password.match(PASSWORD_REGULAR_EXP)) {
-      next(invalidInput("Password must contain at least one upper case letter and at least one number, and must be at least 8 characters or more."))
-      return
-    }
-  }
-  if (!isOlderThan18(new Date(birthday))) {
-    next(invalidInput("You must be 18 or older to signup"))
+  // if (process.env.PASSWORD_RULE !== 'DEVELOP') {
+  //   if (!password.match(PASSWORD_REGULAR_EXP)) {
+  // When password enforcement will be back
+  // need to switch to this password rule in .env.production:
+  // REACT_APP_PASSWORD_RULE=(?=.*[A-Z])(?=.*[0-9])(?=.{8,})
+  // And update invalidInput to the "Password must contain at least...."
+  //     next(invalidInput('Invalid password'))
+  //     return
+  //   }
+  // }
+  if (!verifyAge(new Date(birthday))) {
+    next(invalidInput('You must be 18 or older to signup'))
     return
   }
 
@@ -101,34 +123,31 @@ router.post('/', (req, res, next) => {
     first_name: firstName,
     middle_name: middleName,
     last_name: lastName,
-    email: email,
+    email,
     hashed_password: bcrypt.hashSync(password, 8),
-    gender: gender,
-    birthday: birthday,
+    gender,
+    birthday,
     role: USER_ROLE_REGULAR
   }
-  verifyUserNotInDatabase(userService, email).then(() => {
-    return userService.insert(user)
-  })
+  verifyUserNotInDatabase(userService, email)
+    .then(() => userService.insert(user))
     .then(result => {
       user.id = result.id
       // console.log('user', user)
       const tokenEntry = {
-        email: email,
-        token: crypto.randomBytes(3).toString('hex') //we will need 6 in migrations
+        email,
+        // we will need 6 in migrations
+        token: crypto.randomBytes(3).toString('hex')
       }
       // console.log(tokenEntry);
       // TODO: check if token exists and return error.
       return tokenService.insert(tokenEntry).catch(err => {
-        //if we were unable to insert token, we delete user also.
+        // if we were unable to insert token, we delete user also.
         userService.delete(result.id)
         throw err
       })
         .then(result => {
-          // console.log('result', result)
-          // console.log('result', result)
           profileImageService.insert(user, DEFAULT_USER_PROFILE_IMAGE)
-
           return sendEmail(email, 'Let\'s Go: Account Verification',
             `<html>
               <head>
@@ -142,22 +161,25 @@ router.post('/', (req, res, next) => {
                 </p>
               </body>
             </html>`,
-            'A verification code has been sent to ' + email + '.',
+            `A verification code has been sent to ${email}.`,
             'Error while sending verification email')
         })
-        .then((result) => res.json(result))
+        .then((data) => res.json(data))
         .catch((err) => next(err))
     })
     .catch((err) => {
-      console.log("signup error:", err)
+      console.log('signup error: ', err)
       let status = 500
-      let payload = constructFailure(DATABASE_ERROR, 'Error while inserting new user into database', status)
+      let payload = constructFailure(
+        DATABASE_ERROR,
+        'Error while inserting new user into database',
+        status
+      )
       if (err.isBoom) {
         const data = err.data ? err.data : {}
         status = err.output.statusCode
-        payload =  {...data, ...err.output.payload}
+        payload = { ...data, ...err.output.payload }
       }
-
       next(payload)
     })
 })
@@ -172,7 +194,7 @@ router.patch('/:code', (req, res, next) => {
     code
   } = req.params
   if (!email) {
-    next(invalidInput("Email cannot be blank"))
+    next(invalidInput('Email cannot be blank'))
     return
   }
   // console.log('email', email, 'code', code)
@@ -190,18 +212,22 @@ router.patch('/:code', (req, res, next) => {
       userService.update(user).then(resultUserUpdate => {
         tokenService.delete(code)
         // console.log('result', resultUserUpdate)
-        return res.status(200).send({user: user, token: checkTokenResult.token})
+        return res.status(200).send({ user, token: checkTokenResult.token })
       })
     })
     .catch((err) => {
-      console.log("verify code error:", err)
-      let payload = constructFailure(DATABASE_ERROR, 'Error while inserting new user into database', 500)
+      console.log('verify code error:', err)
+      let payload = constructFailure(
+        DATABASE_ERROR,
+        'Error while inserting new user into database',
+        500
+      )
       if (err.err === TOKEN_EXPIRED) {
         tokenService.delete(code)
         payload = constructFailure(TOKEN_EXPIRED, 'The code has expired.', 401)
       } else if (err.isBoom) {
         const data = err.data ? err.data : {}
-        payload =  {...data, ...err.output.payload}
+        payload = { ...data, ...err.output.payload }
       }
       next(payload)
     })
